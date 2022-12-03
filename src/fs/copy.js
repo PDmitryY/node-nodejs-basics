e6c32d887785811426af20d4d26986e66e8e4c34
Dmitry <p.dmitry.y@gmail.com>
2022-12-04 00:27:52 +0300

    feat: make fs:copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,46 @@
+import fs from 'fs/promises';
+import path from 'path';
+import {fileURLToPath} from 'url'
+
+const basePath = path.dirname(fileURLToPath(import.meta.url));
+const dirPathFrom = path.join(basePath, 'files');
+const dirPathTo = path.join(basePath, 'files-copy');
+
+const makeDir = async (distFolderPath) => {
+  try {
+    await fs.mkdir(distFolderPath);
+  } catch (e) {
+    throw new Error('FS operation failed');
+  }
+}
+
+const readDir = async (dirPathFrom) => {
+    try {
+        const files = await fs.readdir(dirPathFrom, {withFileTypes: true});
+        for(const file of files) {
+        if (file.isFile()) {
+            await fs.copyFile(path.join(dirPathFrom, file.name), path.join(dirPathTo, file.name));
+        } else if(file.isDirectory()) {
+            await copyDir(`${dirPathFrom}/${file.name}`, `${dirPathTo}/${file.name}`);
+        }
+    }
+    } catch (err) {
+        throw new Error('FS operation failed')
+    }
+}
+
+const copyDir = async (dirPathFrom, dirPathTo) => {
+  try {
+    await makeDir(dirPathTo);
+    await readDir(dirPathFrom);
+    
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
+}
+
 const copy = async () => {
-    // Write your code here 
+    await copyDir(dirPathFrom, dirPathTo);
 };
 
 copy();
