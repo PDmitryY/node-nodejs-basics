a0717833911293ceff48f919c4b154ae048ea434
Dmitry <p.dmitry.y@gmail.com>
2022-12-06 01:22:01 +0300

    feat: make compress and decompress tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import path from 'path';
+import {fileURLToPath} from 'url';
+import { pipeline } from 'stream';
+import { createUnzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
 const decompress = async () => {
-    // Write your code here 
+    const basePath = path.dirname(fileURLToPath(import.meta.url));
+    const unCompressedFile = path.join(basePath, 'files', 'fileToCompress.txt');
+    const compressedFile = path.join(basePath, 'files', 'archive.gz');
+    const readStream = createReadStream(compressedFile);
+    const writeStream = createWriteStream(unCompressedFile);
+    const unCompress = createUnzip();
+    pipeline(readStream, unCompress, writeStream, (err)=> console.log(err))
 };
 
 await decompress();
