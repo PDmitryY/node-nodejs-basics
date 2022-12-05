3e137c4f6106bfee9c02e651587db343c961e5d2
Dmitry <p.dmitry.y@gmail.com>
2022-12-06 00:02:40 +0300

    feat: make streams:read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import {fileURLToPath} from 'url'
+import { createReadStream } from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const basePath = path.dirname(fileURLToPath(import.meta.url));
+        const fileToRead = path.join(basePath, 'files', 'fileToRead.txt');
+        const readStream = createReadStream(fileToRead, 'utf-8');
+        readStream.on('data', (data)=>{
+            process.stdout._write(data)
+        })
+      } catch (err) {
+        throw err;
+    }
 };
 
 await read();
