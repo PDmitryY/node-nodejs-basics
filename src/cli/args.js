f4b69b277cb93f73f67be20ad68993e49346d2e7
Dmitry <p.dmitry.y@gmail.com>
2022-12-05 21:04:02 +0300

    feat: make cli:args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+    console.log(process.argv.slice(2).reduce((acc, val, i, arr)=>{
+        if(val.startsWith('--') && arr[i + 1]){
+            acc.push(`${val.slice(2)} is ${arr[i + 1]}`);
+        }
+        return acc;
+    }, []).join(', '))
 };
 
-parseArgs();
+parseArgs()
